ae137e7215d68fdd5d5f4a04fd3558f50a086c47
Charles Zhou <zhou@linux.com>
2023-04-04 23:31:13 -0500

    Boilerplate with tailwind, eslint, and tremor.

---
+++ src/pages/index.tsx
@@ -0,0 +1,91 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>SkyNet Telemetry System</title>
+        <meta name="description" content="SkyNet is a rocket telemetry system designed by NUSTARS." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>src/pages/index.tsx</code>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{' '}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+          <div className={styles.thirteen}>
+            <Image
+              src="/thirteen.svg"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+          </div>
+        </div>
+
+        <div className={styles.grid}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={inter.className}>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              Find in-depth information about Next.js features and&nbsp;API.
+            </p>
+          </a>
+
+          <Card className="max-w-xs mx-auto">
+            <Text>Rocket Altitude</Text>
+            <Metric>71,465 ft</Metric>
+            <Flex className="mt-4">
+              <Text>32% of the target apogee</Text>
+              <Text>225,000 ft</Text>
+            </Flex>
+            <ProgressBar percentageValue={32} className="mt-2" />
+          </Card>
+        </div>
+      </main>
+    </>
+  )
+}
